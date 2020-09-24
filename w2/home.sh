1dfd9b40812cb397e0117bb0e072709ef8a0a40a
maxlaimon <nikitmk@yandex.ru>
2020-09-25 00:39:18 +0300

    add 5 scripts

---
+++ w2/home.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir /hellohome
+cd /hellohome
+echo "hello home!!" > home.txt
+cat home.txt
+ls -l home.txt
+
---
+++ w2/parent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ..
+mkdir helloparent
+echo "hello parent!!" > helloparent/parent.txt
+cat helloparent/parent.txt
+ls -l helloparent/parent.txt
+
