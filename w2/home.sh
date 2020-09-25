56dc1f6c36ecf3ec0837277d568c5c776002002c
maxlaimon <nikitmk@yandex.ru>
2020-09-25 13:27:50 +0300

    removed incorrect scripts

--- w2/home.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir /hellohome
-cd /hellohome
-echo "hello home!!" > home.txt
-cat home.txt
-ls -l home.txt
-
--- w2/parent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
-mkdir helloparent
-echo "hello parent!!" > helloparent/parent.txt
-cat helloparent/parent.txt
-ls -l helloparent/parent.txt
-
