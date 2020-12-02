c3ab7d1619a2f3bf1a851b96148f9216885db7bd
maxlaimon <nikitmk@yandex.ru>
2020-12-02 14:30:38 +0300

    fix 3.py

--- w11/3.py
+++ w11/3.py
@@ -2,27 +2,31 @@ import aiohttp
 import asyncio
 import aiofile
 
-async def get_response(url):
+async def fetch(url):
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
 
             html = await response.text()
-            html = re.split(r'[\n]', html)
-            async with aiofile.async_open("found.txt", 'w+') as found:
-                for line in lines:
-                    if line.startswith("<a >"):
-                        found.writer(line)
+            return html
 
 
-async def find_line(urls):
-#    async with aiofile.async_open(urls, 'r') as urlslist:
-    async for line in aiofile.LineReader(urls):
-        asyncio.ensure_future(fetch(line))
+async def find_line(text):
+    lines = text.split(r'\n')
+    async with aiofile.AIOFile("found.txt", 'w') as found:
+        for line in lines:
+            if line.startswith("<a >"):
+                await found.write(line)
 
 
-if __name__ == "__main__":
+async def seeker(file):
+    async with aiofile.AIOFile(file, 'r') as urls:
+        texts = []
+        async for url in aiofile.LineReader(urls):
+            texts.append(fetch(url))
+        await asyncio.gather(*texts)
 
-    loop = asyncio.get_event_loop()
-    with open('urls.txt', 'r') as urls:
-        loop.run_until_complete(find_line(urls))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(seeker("urls.txt"))
+
